bf599122db892a179d96d4c701f1ee0d190f1dfb
Samuel Chávez <samuelchvez@gmail.com>
2018-10-15 18:30:58 -0600

    Custom actions and authentication

--- sem11/test_api/canoneras/views.py
+++ sem11/test_api/canoneras/views.py
@@ -1,8 +1,17 @@
 # -*- coding: utf-8 -*-
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 from . import models, serializers
 
 
 class CanoneraModelViewSet(viewsets.ModelViewSet):
     queryset = models.Canonera.objects.all()
     serializer_class = serializers.CanoneraSerializer
+
+    @action(methods=['GET'], detail=False, url_path='say-hello')
+    def say_hello(self, request):
+        return Response({
+            'message': "Hola!!"
+        })
--- sem11/test_api/test_api/urls.py
+++ sem11/test_api/test_api/urls.py
@@ -1,8 +1,11 @@
 """ . """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include # , url
 from rest_framework.routers import DefaultRouter
+from rest_framework_jwt.views import obtain_jwt_token
+
 from canoneras import views as canoneras_views
+
 
 
 # Router creation
@@ -16,5 +19,6 @@ router.register(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include(router.urls))
+    path('api/v1/', include(router.urls)),
+    path('api/v1/token-auth/', obtain_jwt_token),
 ]
